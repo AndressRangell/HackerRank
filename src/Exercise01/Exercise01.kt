4e14dbedd4d43f0b853c3f59e104cf8e6fff2b62
Andres Rangel <andresrangel@wposs.com>
2022-01-17 12:17:22 -0500

    Ejercicio 01 finalizado - Contar número de pares de calcetines por color

--- src/Exercise01/Exercise01.kt
+++ src/Exercise01/Exercise01.kt
@@ -1,5 +1,25 @@
 package Exercise01
 
-fun main() {
-    
+fun main(args: Array<String>) {
+    //10 20 20 10 10 30 50 10 20
+    val socks = readLine()!!.trimEnd().split(" ").map{ it.toInt() }.toTypedArray()
+    val pairs = sockMerchant(socks)
+
+    println(pairs)
+}
+
+fun sockMerchant(socks: Array<Int>): Int {
+    val sockMap = mutableMapOf<Int, Int>()
+    var count = 0
+    socks.forEach { sock ->
+        if(sockMap.contains(sock)){
+            sockMap[sock] = sockMap.getValue(sock) + 1
+        }else{
+            sockMap[sock] = 1
+        }
+    }
+    sockMap.forEach { sock ->
+        count += sock.value / 2
+    }
+    return count
 }
